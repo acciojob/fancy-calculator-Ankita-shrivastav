8c4bdcae79e484844d15cfc1e01bab6d1ad65ab1
Ankita-shrivastav <93003257+Ankita-shrivastav@users.noreply.github.com>
2023-12-16 00:12:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+ var firstInput=prompt("Enter the first number:");
+var multipliedResult=firstInput*2;
+
+var secondInput=("Enter the second number:");
+var finalResult=multipliedResult+parseInt(secondInput);
+alert("The final result is:"+ finalResult);
